a4a41362fbbbd60b091ebaaa894a19ca6a31a3e6
Nyfong <litongfong12@gmail.com>
2025-09-25 11:38:02 +0700

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
 from fake_db import fake_items  # Import the fake_db list
-app = FastAPI()
+from pydantic import BaseModel
 
+app = FastAPI()
+# Pydantic model for input (only name, as id is auto-incremented)
+class ItemCreate(BaseModel):
+    name: str
+
+# Pydantic model for output (includes id and name)
+class Item(BaseModel):
+    id: int
+    name: str
+    
+    
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -23,3 +34,15 @@ async def scalar_html():
 @app.get("/items")
 async def read_items():
     return fake_items
+
+#post 
+
+@app.post("/items")
+async def create_item(item: ItemCreate):
+    # Auto-increment ID: Find the maximum ID in fake_db and add 1
+    max_id = max([item["id"] for item in fake_items], default=0)
+    new_id = max_id + 1
+    # Create new item dictionary
+    new_item = {"id": new_id, "name": item.name}
+    fake_items.append(new_item)
+    return new_item
---
+++ models/user_model.py
@@ -0,0 +1,1 @@
+print("user_model.py")
